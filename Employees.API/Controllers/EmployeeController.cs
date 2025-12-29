cede06bdb58bddf0d2c5bc8f0325bf74ff7ebb75
IT Training <it1@tanmeyah.lan>
2025-12-29 01:45:57 -0800

    new codes today

--- Employees.API/Controllers/EmployeeController.cs
+++ Employees.API/Controllers/EmployeeController.cs
@@ -69,9 +69,9 @@ namespace Employees.API.Controllers
 
         [HttpGet]
         [Authorize(Roles = "Viewer,HR,Admin")]
-        public async Task<IActionResult> GetAllEmployees(string? search)
+        public async Task<IActionResult> GetAllEmployees(string? search, int page, int pageSize)
         {
-            int page = 1, pageSize = 10;
+            //int pageSize = 10;
             bool isLoggedIn = User?.Identity?.IsAuthenticated ?? false;
             var result = await _mediator.Send(new GetAllEmployeesQuery(isLoggedIn, search, page, pageSize));
             if (result == null || !result.Any()) return NotFound("No employees found.");
